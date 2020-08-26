ae184cddc505a13f607ff9b21365000f1c1d5164
Prashant <prashantbedi241197@gmail.com>
2020-08-26 15:54:50 +0530

    [Prashant] Add. Connected with database

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/gofiber/fiber v1.14.4
 	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
 	github.com/spf13/viper v1.7.1
 )
--- server.go
+++ server.go
@@ -2,30 +2,51 @@ package main
 
 import (
 	// "net/http"
-	"calculator/service" 
+	"calculator/database"
+	"calculator/service"
+
 	// "github.com/gorilla/mux"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-func main() {
+// Used to set routes
 
+func setupRoutes(app *fiber.App) {
+	// Used with fiber
+	app.Post("/sum", service.Sum)
+	app.Post("/sub", service.Sub)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("postgres", "host=localhost port=5432 user=prashantbedi dbname=calculator password=postgres sslmode=disable")
+	if err != nil {
+		panic("Failed to connect to database. Reason: "+ err.Error())
+	}
+}
+
+func main() {
+	defer database.DBConn.Close()
+	
 	// Using fiber
 
 	var router = fiber.New()
 
-	router.Post("/sum", service.Sum)
-	router.Post("/sub", service.Sub)
+	initDatabase()
 
-	
+	setupRoutes(router)
+
 	// Using mux
-	
+
 	// var router = mux.NewRouter()
 	// router.HandleFunc("/sum",service.Sum).Methods(http.MethodPost)
 	// router.HandleFunc("/sub",service.Sub).Methods(http.MethodPost)
 
 	// Using default library
 
-    // http.HandleFunc("/sum", service.Sum)
+	// http.HandleFunc("/sum", service.Sum)
 	// http.HandleFunc("/sub", service.Sub)
 
 	// Used with default and mux library
@@ -33,6 +54,6 @@ func main() {
 	// http.ListenAndServe(":8090", router)
 
 	// Used with fiber library
-	
+
 	router.Listen(8080)
 }
